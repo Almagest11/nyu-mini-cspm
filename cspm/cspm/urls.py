6e719afa185cd76d1208aa38f6ab80e683553f71
Erik Thompson (et2375) <et2375@nyu.edu>
2024-04-24 23:13:42 -0700

    Updates to our mini CSPM

--- cspm/cspm/urls.py
+++ cspm/cspm/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.views import static
 
 urlpatterns = [
+    path('assets/<path:path>/', static.serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
 ]
